63321e91ed3d48bc2ae81459bfcd6dae9004b529
Victor <victormel87@gmail.com>
2016-04-04 22:33:03 +0200

    Añadido constructor Segmento

--- Segmento.java
+++ Segmento.java
@@ -10,6 +10,8 @@ public class Segmento
     // instance variables - replace the example below with your own
     private int posX;
     private int posY;
+    private int posFinX;
+    private int posFinY;
     private int direccion;
     private Color color;
     private static final int LONGITUD_SEGMENTO = 4;
@@ -17,9 +19,28 @@ public class Segmento
     /**
      * Constructor for objects of class Segmento
      */
-    public Segmento()
+    public Segmento(int posX, int posY, int direccion, Color color)
     {
-       
+        this.posX = posX;
+        this.posY = posY;
+        this.direccion = direccion;
+        this.color = color;
+        if(direccion == 0){ // Direccion 0, hacia la derecha
+            posFinX = posX + LONGITUD_SEGMENTO;
+            posFinY = posY;
+        }
+        else if(direccion == 1){ // Direccion 1, hacia abajo
+            posFinX = posX;
+            posFinY = posY + LONGITUD_SEGMENTO;
+        }
+        else if(direccion == 2){ // Direccion 2, hacia la izquierda
+            posFinX = posX - LONGITUD_SEGMENTO;
+            posFinY = posY;
+        }
+        else if(direccion == 3){ // Direccion 3, hacia arriba
+            posFinX = posX;
+            posFinY = posY - LONGITUD_SEGMENTO;
+        }
     }
 
 }
