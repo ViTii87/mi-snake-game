bd98a7d8408f9e9d9305489825541a7766dae9c3
Victor <victormel87@gmail.com>
2016-04-04 22:18:52 +0200

    Añadidas clases Segmento y Snake y añadidos atributos de Segmento

---
+++ Segmento.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+/**
+ * Write a description of class Segmento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Segmento
+{
+    // instance variables - replace the example below with your own
+    private int posX;
+    private int posY;
+    private int direccion;
+    private Color color;
+    private static final int LONGITUD_SEGMENTO = 4;
+
+    /**
+     * Constructor for objects of class Segmento
+     */
+    public Segmento()
+    {
+       
+    }
+
+}
