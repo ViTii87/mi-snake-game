437b6f8137bbf03c9c4bed8da02e60c40f3feca2
Victor <victormel87@gmail.com>
2016-04-05 11:15:16 +0200

    Contructor de la clase serpiente finalizado

--- Snake.java
+++ Snake.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.awt.Color;
+import java.util.Random;
 /**
  * Write a description of class Snake here.
  * 
@@ -12,6 +13,8 @@ public class Snake
     private ArrayList<Segment> segmentos;
     private static final int NUMERO_SEGMENTOS_INICIALES = 3;
     private static final Color COLOR_SERPIENTE = Color.BLACK;
+    private static final int DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES = 90;
+    private static final int MARGEN_LIENZO = 10;
     private int anchoLienzo;
     private int altoLienzo;
     
@@ -22,7 +25,24 @@ public class Snake
     {
         this.altoLienzo = altoLienzo;
         this.anchoLienzo = anchoLienzo;
+        Canvas lienzo = new Canvas("lienzo", anchoLienzo, altoLienzo);
+        Random rnd = new Random();
         segmentos = new ArrayList<>();
+        for(int i = 1; i <= 3; i++){
+            if(i == 1){
+                segmentos.add(new Segment(anchoLienzo/2, altoLienzo/2, 
+                rnd.nextInt(4)* DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES, COLOR_SERPIENTE));
+            }
+            segmentos.add(new Segment(segmentos.get(i-1).getPosicionFinalX(), 
+                segmentos.get(i-1).getPosicionFinalY(),
+                 rnd.nextInt(4)* DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES,COLOR_SERPIENTE));
+        }
     }
 
+    /**
+     * Metodo para dibujar la serpiente en pantalla
+     */
+    public void dibujar(Canvas lienzo){
+        
+    }
 }
