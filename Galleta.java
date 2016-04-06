b1a3611b85950111e99e4fed4ee3c62b95bf08ef
Victor <victormel87@gmail.com>
2016-04-06 10:10:04 +0200

    Añadida clase Galleta, atributos y constructor

---
+++ Galleta.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Galleta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Galleta
+{
+    // instance variables - replace the example below with your own
+    private int posX;
+    private int posY;
+    public static final int NUM_GALLETAS = 10;
+
+    /**
+     * Constructor for objects of class Galleta
+     */
+    public Galleta(int posX, int posY)
+    {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+   
+}
