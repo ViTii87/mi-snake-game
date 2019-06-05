8422162d077f0f0c27ade67359ff70c0b0f2479d
Victor Melcon <victormel87@gmail.com>
2019-06-05 22:15:58 +0200

    Added laucher class to start game as Java Program

---
+++ Launcher.java
@@ -0,0 +1,12 @@
+/* Lauch java program
+   FileName : "Launcher.java". */
+class Launcher 
+{ 
+    public static void main(String args[]) 
+    { 
+        //SnakeGame mainGame = new SnakeGame();
+        //mainGame.startGame();
+        SnakeGame mainGame = new SnakeGame();
+        mainGame.startGame();
+    } 
+} 
